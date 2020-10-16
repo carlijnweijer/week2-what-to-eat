43e7c17cea30d64a2cacdd643ec7189732f96fce
carlijnweijer <carlijnweijer@gmail.com>
2020-10-17 00:17:04 +0200

    styled Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.scss";
 
 export default function Navbar() {
   return (
-    <div className="navbar">
-      <p>hi</p>
+    <div className="container">
+      <div className="navbar navbarStyle">
+        <NavLink exact to="/" className="smallNavElement">
+          Feed
+        </NavLink>
+        <NavLink exact to="/" className="biggerNavElement">
+          What to eat?!
+        </NavLink>
+        <NavLink exact to="/recipes" className="smallNavElement">
+          Recipes
+        </NavLink>
+      </div>
     </div>
   );
 }
