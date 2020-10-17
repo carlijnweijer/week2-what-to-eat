22345e2091375dacd9069eca6587e60f06905514
carlijnweijer <carlijnweijer@gmail.com>
2020-10-17 20:25:13 +0200

    css RecipeCard

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -9,7 +9,7 @@ export default function RecipeCard(props) {
       <div className="card-body">
         <img src={props.img} alt="" />
         <h5 className="card-title">{props.title}</h5>
-        <p>Cooktime:{props.cooktime}</p>
+        <p>Cooktime: {props.cooktime}</p>
         <a href={props.link} target="_blank">
           <button className="btn btn-success">show me the recipe!</button>
         </a>
